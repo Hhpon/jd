aa814aa27eb37eb2a514f62de2ff06dbf7222c52
Hhpon <624791164@qq.com>
2019-06-13 08:54:22 +0800

    add changePort

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
 let mongoose = require('mongoose');
 
+let env = process.env.NODE_ENV || 'development'
+let dbUrl = 'mongodb://127.0.0.1:20811/jdcx'
+// 开发环境连接测试使用的 MongoDB 服务器
+if (env === 'development') {
+    dbUrl = 'mongodb://127.0.0.1/jdcx'
+}
+
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+
 mongoose.connect('mongodb://localhost:27017/jdcx', { useNewUrlParser: true });  //连接数据库
 
 const articleSchema = new mongoose.Schema({  //schema
